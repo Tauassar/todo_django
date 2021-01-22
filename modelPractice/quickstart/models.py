9786304b9912ea28ecc2e3aa3dcabc0e4d1e624e
tauassar <tauassar.tatiyev@nu.edu.kz>
2021-01-23 00:24:39 +0700

    Initial commit

---
+++ modelPractice/quickstart/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Professor(models.Model):
+    name = models.TextField()
+    surname = models.TextField()
+
+class Student(models.Model):
+    name = models.TextField()
+    surname = models.TextField()
+    personal_tutor = models.ManyToManyField(
+        Professor,
+        related_name='tutoring'
+    )
+
